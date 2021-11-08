939f1e83a4ca4f70587218b8821b01e3f794a818
Sketch-glitch <72163561+Sketch-glitch@users.noreply.github.com>
2021-11-08 22:36:20 +0530

    navbar half section done

---
+++ Client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import Sectionone from "./sectionone/Sectionone";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Sectionone />
+    </div>
+  );
+};
+
+export default Home;
---
+++ Client/src/components/sectionone/Sectionone.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./sectionone.css";
+
+const Sectionone = () => {
+  return (
+    <>
+      <div className="image1"></div>
+      <div class="bg-text">
+        <h1 class="fst-italic">Finderr</h1>
+        <p>Project Team Finding Website</p>
+      </div>
+    </>
+  );
+};
+
+export default Sectionone;
