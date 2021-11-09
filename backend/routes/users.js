0476a84b1770f6995692d2b380643a78ffff2084
Pallavi79 <pallavi3749@gmail.com>
2021-11-09 22:52:49 +0530

    userSchema created

---
+++ backend/routes/users.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+
+router.post('/signin',(req,res)=>{
+
+});
+
+router.post('/signup',(req,res)=>{
+
+});
+
+
+
+
+module.exports=router;
