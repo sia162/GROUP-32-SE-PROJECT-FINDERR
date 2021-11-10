3e8713cf94d60af22e53bee9927262189609aec0
Pallavi79 <pallavi3749@gmail.com>
2021-11-10 15:01:17 +0530

    post request for signup done

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,7 +7,38 @@ router.post('/signin',(req,res)=>{
 });
 
 router.post('/signup',(req,res)=>{
+  User.findOne({email:req.body.email})
+  .exec((error,user)=>{
+    if(user) return res.status(400).json({
+      message:'User already exists '
+    });
 
+    const{
+      username,
+      email,
+      password,
+      techskills
+    }=req.body;
+    const _user = new User({
+      username,
+      email,
+      password,
+      techskills
+    })
+
+    _user.save((error,data) =>{
+      if(error){
+        return res.status(400).json({
+          message: 'Something went wrong'
+        });
+      }
+      if(data){
+        return res.status(201).json({
+          user:data
+        })
+      }
+    })
+  })
 });
 
 
