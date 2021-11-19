752c126fc048a3bd136087e2b411e1dff5d833c5
sia162 <nowitissiya@gmail.com>
2021-11-19 17:14:10 +0530

    register page integrated with backend

--- Client/src/components/register/Register.js
+++ Client/src/components/register/Register.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import registerimg from "./register-img.jpg";
 import "./register.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [usercreds, setUsercreds] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(false);
+  const [errormsg, setErrormsg] = useState("");
+
+  const onchange = (e) => {
+    setUsercreds({ ...usercreds, [e.target.name]: e.target.value });
+    setError(false);
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const { firstName, lastName, username, email, password } = usercreds;
+
+    const response = await fetch("http://localhost:2000/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+      }),
+    });
+
+    const json = await response.json();
+    console.log(json);
+
+    if (json.error) {
+      setError(true);
+      setErrormsg(json.error);
+    } else {
+      setError(false);
+      //save the auth token and redirect
+      localStorage.setItem("token", json.token);
+      navigate("/login");
+    }
+  };
+
   return (
     <div className="register">
       <div className="register-input-img">
@@ -13,22 +64,48 @@ const Register = () => {
       <div className="register-form-right-side">
         <div className="register-form-title">Join us now</div>
 
-        <form className="register-form-group">
+        <form className="register-form-group" onSubmit={handleSignup}>
           <div className="register-input-box">
-            <label htmlFor="username">username </label>{" "}
-            <input type="text" name="username" />
+            <label htmlFor="firstName">firstName </label>{" "}
+            <input
+              type="text"
+              name="firstName"
+              value={usercreds.firstName}
+              onChange={onchange}
+            />
           </div>
           <div className="register-input-box">
+            <label htmlFor="lastName">lastName </label>{" "}
+            <input
+              type="text"
+              name="lastName"
+              value={usercreds.lastName}
+              onChange={onchange}
+            />
+          </div>
+
+          <div className="register-input-box">
             <label htmlFor="email">email </label>{" "}
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              value={usercreds.email}
+              onChange={onchange}
+            />
           </div>
           <div className="register-input-box">
             <label htmlFor="password">password </label>{" "}
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              value={usercreds.password}
+              onChange={onchange}
+            />
           </div>
+          <button type="submit" className="register-btn">
+            Signup
+          </button>
         </form>
-
-        <button className="register-btn">Signup</button>
 
         <div className="orloginlink">
           <Link className="link" to="/login">
@@ -36,6 +113,10 @@ const Register = () => {
             or login here.
           </Link>
         </div>
+
+        {error && (
+          <p style={{ textAlign: "center", color: "red" }}>{errormsg}!</p>
+        )}
       </div>
     </div>
   );
--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,18 @@ const cors = require("cors");
 //routes
 const authRoutes = require("./routes/auth");
 
-
-
 //environment variable or you can say constants
 env.config();
 
 // mongodb connection
 //mongodb+srv://root:<password>@cluster0.8pl1w.mongodb.net/<dbname>?retryWrites=true&w=majority
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.kew9p.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
-).then(()=>{
-  console.log('database conneted');
-})
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.kew9p.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("database conneted");
+  });
 
 app.use(cors());
 app.use(express.json());
